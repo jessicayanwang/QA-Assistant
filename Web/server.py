9c87a6cb2a0d7a609d964d0a3bbf900df57bbe02
Jessica_Wang <jessicayanwang22@gmail.com>
2023-10-05 10:40:35 -0400

    Updated frontend and backend

--- Web/server.py
+++ Web/server.py
@@ -3,18 +3,26 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
 # Initial text
-current_text = "red"
+current_text = "Initial Text"
 
-@app.route('/', methods=['GET', 'POST']) 
-def contact():
-    global current_text 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    if request.method == 'POST':
-        if 'blue' in request.form:
-            current_text = 'blue'  # change title to 'blue'
-        elif 'green' in request.form:
-            current_text = 'green'  # change title to 'green'
-    return render_template('index.html', text=current_text)
+@app.route('/get_target_text', methods=['GET'])
+def get_target_text():
+    global current_text
+    return jsonify({"target_text": current_text})
+
+@app.route('/set_target_text', methods=['POST'])
+def set_target_text():
+    global current_text
+    new_text = request.json.get('target_text')
+    if new_text is not None:
+        current_text = new_text
+        return jsonify({"message": "Target text updated successfully"})
+    else:
+        return jsonify({"error": "Invalid request format"}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
